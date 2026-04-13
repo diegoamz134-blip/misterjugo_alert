6dfd2f571a633819b22380205a947afd86fa1178
Diegomz1303 <diegoamz123@gmail.com>
2026-04-13 00:57:35 -0500

    chore: actualizar config de Android y dependencias

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2203335d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c8pf97a2s2g"
+    "revision": "0.82c34gq8t48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
